9c73dbe0fc4c8583953a75c3ff780afad7fd2d3f
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-08-31 18:05:02 -0400

    Remove Replit configuration files and update server port to use environment variable.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { Timer, scrapeURL, parseHTML, aiParseHTML } from "./lib.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World. The app is, in fact, running. Freaking sick!");
